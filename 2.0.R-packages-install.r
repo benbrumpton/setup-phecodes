2d3bbc5ed398675d5542e6f52e123871e71ccf2b
Ben Brumpton <benb@hunt-genes-home.novalocal>
2020-02-24 14:33:39 +0100

    add R packages phecodes pipeline

---
+++ 2.0.R-packages-install.r
@@ -0,0 +1,28 @@
+#!/usr/bin/env Rscript
+#install required R packages for SAIGE
+
+req_packages <- c("R.utils", "Rcpp", "RcppParallel", "RcppArmadillo", "data.table", "RcppEigen", "Matrix", "methods", "BH", "optparse", "SPAtest", "MetaSKAT", "SKAT", "roxygen2", "rversions","devtools")
+for (pack in req_packages) {
+    if(!require(pack,character.only = TRUE)) {
+        install.packages(pack, repos = "http://cran.us.r-project.org")
+    }
+}
+
+#install required R packages for phecodes
+
+req_packages <- c("data.table", "tidyr", "parallel", "intervals", "XML", "RCurl", "rlist", "bitops")
+for (pack in req_packages) {
+    if(!require(pack,character.only = TRUE)) {
+        install.packages(pack, repos = "http://cran.us.r-project.org")
+    }
+}
+
+#install required R packages for analysis scripts
+
+req_packages <- c("optparse")
+for (pack in req_packages) {
+    if(!require(pack,character.only = TRUE)) {
+        install.packages(pack, repos = "http://cran.us.r-project.org")
+    }
+}
+
