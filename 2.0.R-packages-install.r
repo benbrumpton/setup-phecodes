00d02e0839e19aeb4c6ff7bc97a78cf365037b66
benbrumpton <bmbrumpton@gmail.com>
2020-02-24 14:50:16 +0100

    specify SPAtest 3.0.0

--- 2.0.R-packages-install.r
+++ 2.0.R-packages-install.r
@@ -1,12 +1,15 @@
 #!/usr/bin/env Rscript
 #install required R packages for SAIGE
 
-req_packages <- c("R.utils", "Rcpp", "RcppParallel", "RcppArmadillo", "data.table", "RcppEigen", "Matrix", "methods", "BH", "optparse", "SPAtest", "MetaSKAT", "SKAT", "roxygen2", "rversions","devtools")
+req_packages <- c("R.utils", "Rcpp", "RcppParallel", "RcppArmadillo", "data.table", "RcppEigen", "Matrix", "methods", "BH", "optparse", "MetaSKAT", "SKAT", "roxygen2", "rversions","devtools")
 for (pack in req_packages) {
     if(!require(pack,character.only = TRUE)) {
         install.packages(pack, repos = "http://cran.us.r-project.org")
     }
 }
+
+require(devtools)
+install_version("SPAtest", version = "3.0.0", repos = "http://cran.us.r-project.org")
 
 #install required R packages for phecodes
 
