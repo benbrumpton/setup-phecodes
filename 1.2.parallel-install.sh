5e1bd4f770b8024a6030e2f5b077af925d12bafb
Ben Brumpton <benb@hunt-bristol-home.novalocal>
2020-02-29 22:06:06 +0100

    add my cluster

--- 1.2.parallel-install.sh
+++ 1.2.parallel-install.sh
@@ -1,7 +1,19 @@
 # install
 sudo apt-get install parallel
 
-# add the nodes
+# add the servers to the sshloginfile
+(echo ssh -i ~/.ssh/mykey ubuntu@hunt-bristol-iaas-pollo) > ~/.parallel/my_cluster
+
+# make sure .ssh/config exist
+touch ~/.ssh/config
+cp ~/.ssh/config ~/.ssh/config.backup
+
+# disable StrictHostKeyChecking temporarily
+(echo 'Host *'; echo StrictHostKeyChecking no) >> ~/.ssh/config
+parallel --slf my_cluster --nonall true
+
+# remove the disabling of StrictHostKeyChecking
+mv .ssh/config.backup .ssh/config
 
 
 
